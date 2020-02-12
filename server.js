fbe9c2c62e7877895f8443cf97726739ed3fab3e
pekuhn <peterkuhn92@gmail.com>
2020-02-12 14:42:17 +0100

    sucessfully connected to Waton Tone Analyser. Up next: Integrate into chat

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var connection = mysql.createConnection({
 	password: "pw",
 	database: "web_chat"
 })
-// connect
+// connect to mysql
 connection.connect(function (error) {
 	console.log("MySQL " + error)
 });
@@ -30,16 +30,39 @@ connection.connect(function (error) {
 // socket.io
 var io = require("socket.io")(http)
 
+//request
+const request = require('request')
+
+// PREPARING IBM TONE ANALYSER API-------------
+var api_key = "XWWz1X73jSyN_5tJYMESd7g70En6zSdcwz6xuQdSAQoc"
+var api_url = "https://api.eu-de.tone-analyzer.watson.cloud.ibm.com/instances/f9784a0d-41f5-4895-89ea-76a56492d2be/v3/tone?version=2017-09-21&sentences=false&text="
+
 // CONNECTION EVENT----------------------------
 io.on("connection", function (socket) {
 
 	console.log("New_user with socket_id: ", socket.id)
 	// listen to messages
 	socket.on("new_message", function (data) {
+		console.log("The message is: ", data.message)
+
+		//turn message into url
+		var message_url = encodeURIComponent(data.message)
+		//prepare full api url
+		var url = api_url + message_url
+
+		console.log("api url" + url)
+
+		request.get( url , function (error, response, body) {
+ 			console.error('error:', error); // Print the error if one occurred
+  			console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  			console.log('body:', body); // Print the HTML for the Google homepage.
+		}).auth('apikey', api_key)
+
 		connection.query("INSERT INTO messages(username, message) VALUES('" + data.username + "', '" + data.message + "')", function (error, result) {
         		data.id = result.insertId
         		io.emit("new_message", data)
         	})
+
 	})
-	
+
 })
